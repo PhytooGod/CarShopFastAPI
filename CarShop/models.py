b4b4118390453da52f45e351a2a7d515e8e71f96
Phytoo <52602080+PhytooGod@users.noreply.github.com>
2022-02-27 22:38:23 +0600

    Last commit maybe)

--- CarShop/models.py
+++ CarShop/models.py
@@ -3,7 +3,7 @@ from sqlalchemy import Column, String, Integer, DateTime, ForeignKey
 from sqlalchemy.orm import relationship 
 
 class Car(Base):
-    __tablename__ = "Car"
+    __tablename__ = "car"
     id = Column(Integer, primary_key=True, index=True, unique=True)
     brand = Column(String)
     title = Column(String)
--- main.py
+++ main.py
@@ -3,14 +3,68 @@ from schemas import Car, Owner
 from core.db import SessionLocal
 from typing import List
 import owner.models as models
- 
+import CarShop.models as carmodels
+
 app = FastAPI()
 db = SessionLocal()
+
+@app.get('/cars', response_model=List[Car], status_code=200)
+def get_all_cars():
+    cars = db.query(carmodels.Car).all()
+    return cars
+
+@app.get('/car/{pk}', response_model=Car, status_code=status.HTTP_200_OK)
+def get_car(pk:int):
+    car=db.query(carmodels.Car).filter(carmodels.Car.id==pk).first()
+    return car
+
+@app.post('/cars', response_model=Car, status_code=status.HTTP_201_CREATED)
+def add_car(car:Car):
+    if db.query(models.Owner).filter(models.Owner.id == car.owner).first() is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="No such owner")
+
+    new_car = carmodels.Car(
+        brand = car.brand,
+        title = car.title,
+        date = car.date,
+        owner = car.owner
+    )
+
+    db.add(new_car)
+    db.commit()
+
+    return new_car
+
+
+@app.put('/car/{pk}', response_model=Car, status_code=status.HTTP_200_OK)
+def update_car(pk: int, car: Car):
+    new_car = db.query(carmodels.Car).filter(carmodels.Car.id == pk).first()
+    new_car.name = car.name
+    new_car.surname = car.surname
+    new_car.email = car.email
+    new_car.date = car.date
+    
+    db.commit()
+
+    return new_car
+
+@app.delete('/car/{pk}')
+def delete_car(pk: int):
+    car_to_delete = db.query(carmodels.Car).filter(carmodels.Car.id == pk).first()
+    
+    if car_to_delete is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="No such car")
+    
+    db.delete(car_to_delete)
+    db.commit()
+
+    return car_to_delete
 
 @app.get('/owners',response_model=List[Owner], status_code=200)
 def get_all_owners():
     owners=db.query(models.Owner).all()
     return owners
+
 
 @app.get('/owner/{pk}', response_model=Owner, status_code=status.HTTP_200_OK)
 def get_owner(pk:int):
